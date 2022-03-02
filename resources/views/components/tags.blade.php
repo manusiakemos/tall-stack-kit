9e630e6ff39ccfc64000ab96e07f7817f40ef344
Hafiz <manusiakemos@gmail.com>
2022-03-02 12:50:21 +0800

    update tags component

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -21,7 +21,7 @@
     <div class="flex flex-wrap mt-1">
         <template x-for="(item,index) in items" :key="index">
             <div class="px-2 py-1 mr-1 relative">
-                <div class="w-11/12 absolute left-0 mr-3 bg-primary-500 text-gray-100 rounded-lg px-1 py-1" x-text="item"></div>
+                <div class="w-11/12 left-0 mr-3 bg-primary-500 text-gray-100 rounded-lg px-3 py-1" x-text="item"></div>
                 <button type="button"
                         x-on:click="items = arrayRemove(items, item)"
                         class="rounded-full h-6 w-6 ml-1 bg-primary-600 hover:bg-primary-700 text-gray-100 flex items-center justify-center absolute right-0 top-0 shadow-xl">
