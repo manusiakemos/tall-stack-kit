f97212fe0c499b9a5f2692c6a5eb4fe061538648
Hafiz <manusiakemos@gmail.com>
2022-03-09 01:29:48 +0800

    update modal component

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -43,7 +43,7 @@
      x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
      x-show="show"
      id="{{ $id }}"
-     class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
+     class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50 flex items-center justify-center"
      style="display: none;"
 >
     <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false" x-transition:enter="ease-out duration-300"
