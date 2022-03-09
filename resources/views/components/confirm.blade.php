94ca9fbea98a82943e0f52c1113cd9e0537da1ed
Hafiz <manusiakemos@gmail.com>
2022-03-09 11:57:10 +0800

    update confirm and modal component

--- resources/views/components/confirm.blade.php
+++ resources/views/components/confirm.blade.php
@@ -1,15 +1,15 @@
 @props(['id'])
-<x-kit::modal id="{{ $id }}" max-width="sm" {!! $attributes !!}>
+<x-kit::modal position="center" id="{{ $id }}" max-width="sm" wire:model="{{$attributes->get('wire:model')}}">
     <div class="mx-6 my-3 flex flex-col justify-center items-center overflow-auto">
         <div class="flex flex-col items-center justify-center">
-            <h4 class="font-semibold tracking-wider text-3xl text-primary-500 text-center mb-3">
+            <h4 class="font-semibold text-xl md:text-3xl dark:text-gray-100 text-gray-500 text-center mb-3">
                 {{ $title ?? 'Are you sure?' }}
             </h4>
-            {{ $text??'' }}
+            {{ $text ?? '' }}
         </div>
-        <div class="flex flex-grow w-full px-6 pt-12 mx-6">
-           {{ $yes??'' }}
-           {{ $no??'' }}
+        <div class="flex flex-grow w-full pt-12 md:px-6 md:mx-6">
+           {{ $yes ?? '' }}
+           {{ $no ?? '' }}
         </div>
     </div>
 </x-kit::modal>
--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,4 +1,4 @@
-@props(['id', 'maxWidth'])
+@props(['id', 'maxWidth', 'position' => 'top'])
 @php
     $id = $id ?? $attributes->wire('model')->value();
     $maxWidth = [
@@ -43,19 +43,22 @@
      x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
      x-show="show"
      id="{{ $id }}"
-     class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50 flex items-center justify-center"
+     class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50 {{$position == 'center' ? ' flex justify-center items-center flex-grow' : ''}}"
      style="display: none;"
 >
-    <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false" x-transition:enter="ease-out duration-300"
+    <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false"
+         x-transition:enter="ease-out duration-100"
          x-transition:enter-start="opacity-0"
          x-transition:enter-end="opacity-100"
-         x-transition:leave="ease-in duration-200"
+         x-transition:leave="ease-in duration-100"
          x-transition:leave-start="opacity-100"
          x-transition:leave-end="opacity-0">
-        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        <div class="absolute inset-0 backdrop-filter backdrop-blur-sm backdrop-sepia-0 bg-black/30 dark:bg-white/30"></div>
     </div>
 
-    <div x-show="show" class="mb-6 bg-white dark:bg-gray-800 rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
+    <div x-show="show" class="mb-6 bg-white dark:bg-gray-800 rounded-lg overflow-auto shadow-xl
+                              {{$position == 'center' ? 'grow' : ''}}
+                              transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
          x-transition:enter="ease-out duration-300"
          x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
          x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
