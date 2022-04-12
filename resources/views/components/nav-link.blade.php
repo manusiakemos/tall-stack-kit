cdd0a61c3b8bce582215077aeb5bf99a5939176c
Hafiz <manusiakemos@gmail.com>
2022-04-12 23:56:09 +0800

    nav link

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,4 @@
+<a
+    {{$attributes->merge(['class'=>'tracking-wider ml-3 text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-300 font-semibold uppercase'])}}>
+    {{$slot}}
+</a>
